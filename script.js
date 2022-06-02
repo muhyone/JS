bef03df65b5da351e07304d6287fbd1a2fd56611
MikaHienuki <muhyo0207ss.lc1108sh.yk@gmail.com>
2022-06-02 23:42:46 +0900

    インラインスタイル編
    【課題】
    script.js
    ＞イベント発火によるスタイルの変更処理

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-window.addEventListener('load', function(){
-    const pullDownButton = document.getElementById("lists");
-});
+// window.addEventListener('load', function(){
+//     const pullDownButton = document.getElementById("lists");
+// });
 
-window.addEventListener('mouseover', function(){
-    const pullDownButton = document.getElementById("lists");
-    console.log(pullDownButton);
-});
+// window.addEventListener('mouseover', function(){
+//     const pullDownButton = document.getElementById("lists");
+//     console.log(pullDownButton);
+// });
 
 window.addEventListener('mouseout', function(){
     const pullDownButton = document.getElementById("lists");
     console.log(pullDownButton);
+    pullDownButton.addEventListener('mouseover', function(){
+        pullDownButton.setAttribute("style","background-color:blue");
+    });
+    pullDownButton.addEventListener('mouseout', function(){
+        pullDownButton.removeAttribute("style", "background-color:red;");
+    });
+    pullDownButton.addEventListener('click', function(){
+        pullDownButton.setAttribute("style", "background-color: green;");
+    });
 });
+
