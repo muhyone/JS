3a12741c3402bff8f1258091a1e8410fd86a4867
MikaHienuki <muhyo0207ss.lc1108sh.yk@gmail.com>
2022-06-04 14:26:28 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -7,17 +7,38 @@
 //     console.log(pullDownButton);
 // });
 
+// window.addEventListener('mouseout', function(){
+//     const pullDownButton = document.getElementById("lists");
+//     console.log(pullDownButton);
+//     pullDownButton.addEventListener('mouseover', function(){
+//         pullDownButton.setAttribute("style","background-color:blue");
+//     });
+//     pullDownButton.addEventListener('mouseout', function(){
+//         pullDownButton.removeAttribute("style", "background-color:red;");
+//     });
+//     pullDownButton.addEventListener('click', function(){
+//         pullDownButton.setAttribute("style", "background-color: green;");
+//     });
+// });
+
+
 window.addEventListener('mouseout', function(){
     const pullDownButton = document.getElementById("lists");
-    console.log(pullDownButton);
+    const pullDownParents = document.getElementById("pull-down");
+
     pullDownButton.addEventListener('mouseover', function(){
-        pullDownButton.setAttribute("style","background-color:blue");
+        this.setAttribute("style", "background-color: blue;");
     });
     pullDownButton.addEventListener('mouseout', function(){
-        pullDownButton.removeAttribute("style", "background-color:red;");
+        this.removeAttribute("style", "background-color: red;");
     });
     pullDownButton.addEventListener('click', function(){
-        pullDownButton.setAttribute("style", "background-color: green;");
+        if(pullDownParents.getAttribute("style") == "display: block;"){
+            pullDownParents.removeAttribute("style", "display: block;");
+            console.log("非表示");
+        } else {
+            pullDownParents.setAttribute("style", "display: block;");
+            console.log("表示");
+        };
     });
 });
-
